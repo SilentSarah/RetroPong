5de6ae9ac265738f156d142f2c16920d8633aab2
Sarah Meftah <shmeftah@student.1337.ma>
2024-02-25 20:17:51 +0100

    Front-page Almost Ready, needs small adjustments for mobile

---
+++ Frontend/static/js/animation.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                console.log('Element is visible');
+                entry.target.classList.add('fade_in');
+            }
+        });
+    });
+    let Elements = document.querySelectorAll('.index-card');
+    Elements.forEach(element => {
+        observer.observe(element);
+    });
+});
---
+++ Frontend/static/js/index.js
@@ -0,0 +1,50 @@
+const routes = [
+    {   path: '/404', 
+        component: () => grabContent('/static/content/404.html')
+    },
+    {   path: '/',
+        component: () => grabContent('/static/content/home.html')
+    },
+    {
+        path: '/about',
+        component: () => grabContent('/static/content/about.html')
+    },
+    {
+        path: '/contact',
+        component: () => grabContent('/static/content/contact.html')
+    },
+]
+
+async function grabContent(path) {
+    const response = await fetch(path);
+    if (!response.ok) {
+        return await grabContent('/404.html');
+    }
+    const data = await response.text();
+    return data;
+}
+
+function router() {
+    const path = window.location.pathname;
+    const route = routes.find(route => route.path === path);
+    if (route) {
+        route.component().then(html => {
+            document.getElementById('mainContent').innerHTML = html;
+        });
+    } else {
+        routes[0].component().then(html => {
+            document.getElementById('mainContent').innerHTML = html;
+        });
+    }
+}
+
+window.addEventListener('popstate', router);
+document.querySelectorAll('a').forEach(link => {
+        link.addEventListener('click', function(e) {
+        e.preventDefault();
+        history.pushState(null, null, this.href);
+        router();
+    });
+});
+
+router();
