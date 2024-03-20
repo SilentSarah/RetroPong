1629508c86c9f146c1975eeab9f7041e59434392
Sarah Meftah <sarah@retropong.retropong.games>
2024-03-20 12:26:39 -0500

    Created Generic Microservice container for Retropong Team

---
+++ Backend/RetroPong/AuthenticationDocker/Auth/Configuration/ProjectCheck.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#************************************#
+#   Author:     Sarah Meftah         #
+#   Date:       20/03/2024           #
+#   Version:    1.0                  #
+#   Purpose:    Project Healthcheck  #
+#************************************#
+
+source  /Toolkit/DjangoSetup.sh
+
+check_project_availability() {
+    if [ -d "/VolumeData/$PROJECT_NAME" ]
+    then
+        echo "$PROJECT_NAME is available"
+        if [ -d "/VolumeData/$PROJECT_NAME/$PROJECT_APP" ]
+        then
+            echo "$PROJECT_APP is available"
+        else
+            echo "$PROJECT_APP isn't available creating..."
+            create_app $PROJECT_APP "/VolumeData/$PROJECT_NAME" $PROJECT_NAME
+        fi
+    else
+        echo "Project isn't installed creating..."
+        initiate_django $PROJECT_NAME $PROJECT_APP $SU_NAME $SU_PASS $SU_EMAIL $RP_DB
+    fi
+}
+if [ $SETUP_DONE -eq 1 ]
+then
+    check_project_availability
+fi
+exec $@
---
+++ Backend/RetroPong/AuthenticationDocker/Auth/Configuration/Setup.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+#************************************#
+#   Author:     Sarah Meftah         #
+#   Date:       20/03/2024           #
+#   Version:    1.0                  #
+#   Purpose:    Dependency Setup     #
+#************************************#
+
+install_sys_dependency() {
+    echo "Installing System Dependency $1"
+    apt install $1
+}
+
+check_sys_dependency() {
+    apt list --installed | grep $1 > /dev/null
+    if [ $? -eq 1 ]
+    then
+        echo "System Dependency $1 isn't installed"
+        return 1
+    fi
+    return 0
+}
+
+check_pip_installation() {
+    pip --version > /dev/null
+    if [ $? -eq 0 ]
+    then
+        echo "pip has sucessfuly been installed."
+    else
+        echo "pip hasn't been installed, installing..."
+        apt install python3-pip -y -f
+    fi
+}
+
+check_python_package() {
+    pip freeze | grep $1 > /dev/null
+    if [ $? -eq 1 ]
+    then
+        echo "$1 is installed."
+        return 0
+    fi
+    return 1
+}
+
+install_python_package() {
+    echo "Installing $1"
+    apt install python3-$1 -y -f
+}
+
+SetupDependenciesAndCommands() {
+    echo "Setup is Starting..."
+    sleep 1
+
+    install_sys_dependency "python3"
+    install_python_package "django"
+    install_python_package "psycopg"
+    install_python_package "psycopg2"
+
+    mkdir -p /VolumeData
+}
+
+SetupDependenciesAndCommands
