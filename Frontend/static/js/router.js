71e2d93a17082c26c1a98af6efb10b6cb417ee26
sekak <ahmadesekak@gmail.com>
2024-04-23 15:08:09 +0100

    add arrows photos

--- Frontend/static/js/router.js
+++ Frontend/static/js/router.js
@@ -5,7 +5,7 @@ const routes = [
     },
     {   path: '/',
         on: false,
-        component: () => grabContent('/static/content/dashboard.html')
+        component: () => grabContent('/static/content/chat.html')
     },
     {
         path: '/login',
