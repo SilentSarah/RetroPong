aaf5d3c65951987f162d97f9f3bac920b0a3add1
Sarah Meftah <hichammeftah4@gmail.com>
2024-06-24 17:19:17 +0100

    Fixed env path not being correct for the chat backend

--- Backend/Chat-Backend/backend/backend/settings.py
+++ Backend/Chat-Backend/backend/backend/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from dotenv import dotenv_values
 
-config = dotenv_values("../.env")
+config = dotenv_values("../../.env")
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
