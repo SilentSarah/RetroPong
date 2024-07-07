1e44cd1d94ab3d5856b404525b6e7eaace3efce6
Sarah Meftah <hichammeftah4@gmail.com>
2024-07-07 15:47:55 +0100

    Deleted OLD Game Code.

--- Backend/game/game/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import re_path
-from .consumers import GameConsumer, TournamentConsumer
-
-websocket_urlpatterns = [
-    re_path(r"ws/game/$", GameConsumer.as_asgi()),
-    re_path(r"ws/tournament/$", TournamentConsumer.as_asgi()),
-]
